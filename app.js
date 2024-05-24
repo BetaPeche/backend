059ce33c52bd66e806354802841475b486dd2a01
BetaPeche <lolopellegrino@gmail.com>
2024-05-24 09:36:51 +0200

    Implementation of connexion with database

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const Thing = require('./models/thing')
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vxrbvet.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=${process.env.DB_NAME}`)
+	.then(() => console.log('Connexion à MongoDB réussie !'))
+	.catch((error) => console.log('Connexion à MongoDB échouée !'))
 
 app.use(express.json())
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
 	next();
 })
 
+
 app.post('/api/stuff', (req, res, next) => {
-	console.log(req.body)
-	res.status(201).json({
-		message: 'Thing created successfully!'
+	delete req.body._id;
+	const thing = new Thing({
+		...req.body
 	})
+	thing.save()
+		.then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+		.catch(error => res.status(400).json({ error }))
+})
+
+app.get('/api/stuff/:id', (req, res, next) => {
+	Thing.findOne({ _id: req.params.id })
+		.then(thing => res.status(200).json(thing))
+		.catch(error => res.status(404).json({ error }))
 })
 
 app.get('/api/stuff', (req, res, next) => {
-	const stuff = [
-		{
-			_id: 'oeihfzeoi',
-			title: 'My first thing',
-			description: 'All of the info about my first thing',
-			imageUrl: '',
-			price: 4900,
-			userId: 'qsomihvqios',
-		},
-		{
-			_id: 'oeihfzeomoihi',
-			title: 'My second thing',
-			description: 'All of the info about my second thing',
-			imageUrl: '',
-			price: 2900,
-			userId: 'qsomihvqios',
-		}
-	]
-
-	res.status(200).json(stuff)
+	Thing.find()
+		.then(things => res.status(200).json(things))
+		.catch(error => res.status(400).json({ error }))
 })
 
 module.exports = app
